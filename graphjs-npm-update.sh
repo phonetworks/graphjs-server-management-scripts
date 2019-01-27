2891da3a0dfd2a7f496f7c447717c86dd27fef78
Emre Sokullu <esokullu@gmail.com>
2019-01-27 12:45:22 -0800

    Create graphjs-npm-update.sh

---
+++ graphjs-npm-update.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+cd /var/local/graphjs-website
+npm i
+
+cd /var/local/graphjs-website-8081
+npm i 
+
+cd /var/local/graphjs-website-8082
+npm i
+
+cd /var/local/graphjs-website-8083
+npm i
