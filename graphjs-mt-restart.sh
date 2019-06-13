efa6613a97a02c32d1060e9cae25903e16b725ed
Emre Sokullu <esokullu@gmail.com>
2019-06-12 22:04:16 -0700

    Update graphjs-mt-restart.sh

--- graphjs-mt-restart.sh
+++ graphjs-mt-restart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # this is for mt servers
-supervisorctl stop all && /opt/graphjs-server-mt/killme.sh && supervisorctl start all
+supervisorctl stop all && /opt/graphjs-server-mt/killme.sh
+supervisorctl start all # because the previous command will not terminate in 0
